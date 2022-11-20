42ef821af870770baae4a0ba7f2d7f2b622e3f7e
Jean-Paul Weijers <18416852+jpweijers@users.noreply.github.com>
2022-11-20 12:58:03 +0000

    calculator done

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,28 @@ use std::env::{args, Args};
 
 fn main() {
     let mut args: Args = args();
-    let first = args.nth(0).unwrap();
-    println!("{:?}", first);
+
+    let first = args.nth(1).unwrap();
+    let operator = args.next().unwrap().chars().next().unwrap();
+    let second = args.next().unwrap();
+
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number = second.parse::<f32>().unwrap();
+    
+    let result = operate(operator, first_number, second_number);
+    println!("{:?}", output(first_number, operator, second_number, result));
+
+    fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+        match operator {
+            '+' => first_number + second_number,
+            '-' => first_number - second_number,
+            '/' => first_number / second_number,
+            '*' | 'x' | 'X' => first_number * second_number,
+            _ => panic!("Invalid operator used.")
+        }
+    }
+    
+    fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+        format!("{} {} {} = {}", first_number, operator, second_number, result)
+    }
 }
